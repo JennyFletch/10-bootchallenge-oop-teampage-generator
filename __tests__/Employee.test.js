4018145fdbebb79d324e50cf6d3fcacfadf852b5
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2022-11-25 15:11:20 -0700

    Added dummy tests to establish testing logic

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,12 @@
+// Sample test for reference
+/* test("should confirm 1 + 1 = 2", () => {
+    expect(1 + 1).toBe(2);
+})  */
+
+
+
+test("1 plus 1 should be equal to 2", () => {
+    let a = 1;
+    let b = 1;
+    expect(a + b).toBe(2);
+})
