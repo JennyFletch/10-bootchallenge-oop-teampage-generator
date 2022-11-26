91b99da928787b9e218ff002eda1e715b2c79300
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2022-11-26 01:56:05 -0700

    Reorganized inputs and pulled HTML dynamic data from class objects

--- index.js
+++ index.js
@@ -1,150 +1,27 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 let teamHTML = "";
 
-// Find out if the user wants to add an employee or quit the application
-function addOrExit () {
-    
-    inquirer
-    .prompt([
-        {
-        type: 'list',
-        message: 'What would you like to do?',
-        name: 'employeeRole',
-        choices: ["Add a Manager", "Add an Engineer", "Add an Intern", "Exit"],
-        },
-    ])
-    .then((responses) => {
-
-        if ( responses.employeeRole === "Exit") {
-            writeFileAndQuit();
-        } else {
-            getEmployeeInfo(responses.employeeRole);
-        }
-    });
-}
-
-
-
-
-
-// Get the extra field that matches the employee's job title
-function getEmployeeExtra (employeeRole, employeeName, employeeId, employeeEmail) {
-
-    switch (employeeRole) {
-        case "Manager":
-            getOfficeNumber(employeeRole, employeeName, employeeId, employeeEmail);
-            break;
-        case "Engineer":
-            getGithub(employeeRole, employeeName, employeeId, employeeEmail);
-            break;
-        case "Intern":
-            getSchool(employeeRole, employeeName, employeeId, employeeEmail);
-            break;
-        default:
-            break;
-    }
-}
-
-
-// Get employee's office number - MANAGER
-function getOfficeNumber (employeeRole, employeeName, employeeId, employeeEmail) {
-    
-    inquirer
-    .prompt([
-        {
-        type: 'input',
-        message: 'Employee Office #:',
-        name: 'employeeExtra',
-        validate(value) {
-            if ( value && Number(value) > 0 && Number(value) < 400 ) { return true; }
-            return 'Please enter an office number between 1 and 399.'; 
-        },
-        }
-    ])
-    .then((response) => {
-        var extraString = `Office #: ${ response.employeeExtra }`;
-        createNewUser(employeeRole, employeeName, employeeId, employeeEmail, extraString);
-    });
-}
-
-
-// Get employee's GitHub username - ENGINEER
-function getGithub (employeeRole, employeeName, employeeId, employeeEmail) {
-    
-    inquirer
-    .prompt([
-        {
-        type: 'input',
-        message: 'Employee GitHub User:',
-        name: 'employeeExtra',
-        validate(value) {
-            var githubChars = value.split("");
-            if ( value && !githubChars.includes(" ") ) { return true; }
-            return 'Please enter a valid GitHub username.'; 
-        },
-        },
-    ])
-    .then((response) => {
-        var extraString = `Office #: ${ response.employeeExtra }`;
-        createNewUser(employeeRole, employeeName, employeeId, employeeEmail, extraString);
-    });
-}
-
-
-// Get employee's school - INTERN
-function getSchool (employeeRole, employeeName, employeeId, employeeEmail) {
-    
-    inquirer
-    .prompt([
-        {
-        type: 'input',
-        message: 'Employee School:',
-        name: 'employeeExtra',
-        validate(value) {
-            if ( value ) { return true; }
-            return 'Please enter the school this employee attends.'; 
-        },
-        },
-    ])
-    .then((response) => {
-        var extraString = `Office #: ${ response.employeeExtra }`;
-        createNewUser(employeeRole, employeeName, employeeId, employeeEmail, extraString);
-    });
-}
-
-
-function createNewUser(employeeRole, employeeName, employeeId, employeeEmail, extraString) { 
-
-    let newguy = new Intern(employeeName, employeeId, employeeEmail, employeeRole, employeeExtra); 
-
-    teamHTML += `
-    <div class="employee-block">
-        <div>Employee Name: ${ newguy.employeeName }</div>
-        <div>Employee ID #${ newguy.employeeId }</div>
-        <div>Email: ${ newguy.employeeEmail }</div>
-        <div>Job Title: ${ newguy.employeeRole }</div>
-        <div>${ newguy.employeeExtra}</div>
-    </div>
-    `;
-          
-    // Prompt user to enter another employee or exit the application
-    addOrExit();
-}
-
-
-
 
 // Find out if the user wants to add an employee or quit the application
-function getEmployeeInfo (employeeRole) {
-    
+function getEmployeeInfo () {
+
     inquirer
     .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'employeeRole',
+            choices: ["Add a Manager", "Add an Engineer", "Add an Intern", "Exit"],
+         },
         {
             type: 'input',
             message: 'Employee Name:',
             name: 'employeeName',
+            when: (answers) => !(answers.employeeRole === 'Exit'),
             validate(value) {
                 if (value.length) { 
                     return true 
@@ -156,6 +33,7 @@ function getEmployeeInfo (employeeRole) {
             type: 'input',
             message: 'Employee ID:',
             name: 'employeeId',
+            when: (answers) => !(answers.employeeRole === 'Exit'),
             validate(value) {
                 if(value.length) {
                     var idString = value.split("");
@@ -168,24 +46,95 @@ function getEmployeeInfo (employeeRole) {
             type: 'input',
             message: 'Employee Email:',
             name: 'employeeEmail',
+            when: (answers) => !(answers.employeeRole === 'Exit'),
             validate(value) {
                 var emailChars = value.split("");
                 if (value.length && emailChars.includes("@") && emailChars.includes(".")) { return true; }
                 return 'Please enter a valid email address.'; 
             }
          },
+         {
+            type: 'input',
+            message: 'Employee Office #:',
+            name: 'employeeExtra',
+            when: (answers) => !(answers.employeeRole === 'Exit') && answers.employeeRole === 'Add a Manager',
+            validate(value) {
+                if ( value.length && Number(value) > 0 && Number(value) < 400 ) { return true; }
+                return 'Please enter an office number between 1 and 399.'; 
+            }
+         },
+         {
+            type: 'input',
+            message: 'Employee GitHub User:',
+            name: 'employeeExtra',
+            when: (answers) => !(answers.employeeRole === 'Exit') && answers.employeeRole === 'Add an Engineer',
+            validate(value) {
+                var githubChars = value.split("");
+                if ( value.length && !githubChars.includes(" ") ) { return true; }
+                return 'Please enter a valid GitHub username.'; 
+            }
+         },
+         {
+            type: 'input',
+            message: 'Employee School:',
+            name: 'employeeExtra',
+            when: (answers) => !(answers.employeeRole === 'Exit') && answers.employeeRole === 'Add an Intern',
+            validate(value) {
+                if ( value.length ) { return true; }
+                return 'Please enter the school this employee attends.'; 
+            }
+         },
     ])
-    .then((response) => {
+    .then((responses) => {
 
-        var eRole = employeeRole;
-        var eName = response.employeeName;
-        var eId = response.employeeId;
-        var eEmail = response.employeeEmail;
+        if (responses.employeeRole === "Exit") {
+            writeFileAndQuit();
+        } else {
 
-        getEmployeeExtra(eRole, eName, eId, eEmail);
+        var eRole = responses.employeeRole;
+        var eName = responses.employeeName;
+        var eId = responses.employeeId;
+        var eEmail = responses.employeeEmail;
+        var eExtra = responses.employeeExtra;
+        
+        createNewUser(eRole, eName, eId, eEmail, eExtra);
+        }
     });
 }
 
+
+function createNewUser(employeeRole, employeeName, employeeId, employeeEmail, employeeExtra) { 
+    
+    switch (employeeRole) {
+        case "Add a Manager":
+            var newguy = new Manager(employeeName, employeeId, employeeEmail, employeeExtra); 
+            var eExtraString = `Office #: ${ newguy.getOffice() }`;
+            break;
+        case "Add an Engineer":
+            var newguy = new Engineer(employeeName, employeeId, employeeEmail, employeeExtra); 
+            var eExtraString = `GitHub User: <a href="https://github.com/${ newguy.getGithub() }/" target="_blank">https://github.com/${ newguy.getGithub() }/</a>`;
+            break;
+        case "Add an Intern":
+            var newguy = new Intern(employeeName, employeeId, employeeEmail, employeeExtra); 
+            var eExtraString = `Current School: ${ newguy.getSchool() }`;
+            break;
+        default:
+            break;
+    } 
+
+    teamHTML += `
+    <div class="employee-block">
+        <div>Employee Name: ${ newguy.getName() }</div>
+        <div>Employee ID #${ newguy.getId() }</div>
+        <div>Email: <a href="mailto:${ newguy.getEmail() }?subject=Attention%20Team%20Members">${ newguy.getEmail() }</a></div>
+        <div>Job Title: ${ newguy.getRole() }</div>
+        <div>${ eExtraString }</div>
+    </div>
+    `;
+          
+    // Prompt user to enter another employee or exit the application
+    getEmployeeInfo();
+}
 
 
 // If user is done adding employees, create HTML page and quit application
@@ -206,10 +155,13 @@ function writeFileAndQuit() {
 
     const htmlSnippet = startHTML + '\n' + teamHTML + '\n' + endHTML;
 
-    fs.writeFile('./dist/index.html', `${htmlSnippet}`, (err)=>{
-    return console.log("done!");
-    } );
+    //fs.writeFile('./dist/index.html', `${htmlSnippet}`, (err)=>{
+    //return console.log("done!");
+    //} );
 
+    fs.writeFile('./dist/index.html', `${htmlSnippet}`, (err)=>
+        err ? console.error(err) : console.log('\nSuccess!')
+    );
 }
 
-addOrExit(); 
+getEmployeeInfo(); 
